ec52307cf7ca16bc26286605fea966e0a90c7697
monzag <zaganiacz.m@gmail.com>
2017-11-26 23:55:26 +0100

    Add app

---
+++ src/main/java/com/monzag/App.java
@@ -0,0 +1,19 @@
+package com.monzag;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
+public class App
+{
+    public static void main( String[] args ) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8111), 0);
+
+        server.createContext("/login", new LoginHandler());
+        server.createContext("/hello", new Hello());
+        server.createContext("/static", new Static());
+        server.setExecutor(null);
+
+        server.start();
+    }
+}
