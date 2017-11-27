b6416a7bbada07811389d5d6303149f65a641836
monzag <zaganiacz.m@gmail.com>
2017-11-27 02:25:46 +0100

    Change port

--- src/main/java/com/monzag/App.java
+++ src/main/java/com/monzag/App.java
@@ -7,7 +7,7 @@ import java.net.InetSocketAddress;
 public class App
 {
     public static void main( String[] args ) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8111), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8101), 0);
 
         server.createContext("/login", new LoginHandler());
         server.createContext("/static", new Static());
